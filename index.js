7ac39337efb4a180bc9ea248fc773bea0ad88285
Kyle Daigle <kyle.daigle@github.com>
2016-04-08 14:16:31 -0400

    Initial commit of IRC issue templates

---
+++ index.js
@@ -0,0 +1,22 @@
+var irc = require("irc"),
+  handlebars = require("handlebars"),
+  templates = require("./templates/issue");
+
+exports.handler = function(event, context) {
+  var client = new irc.Client("irc.freenode.net", "kdaigleBotTest", {
+    debug: true,
+    autoConnect: false,
+  });
+  var compiledTemplate = handlebars.compile(templates.rawIssueTemplate);
+  var compiledMessage = compiledTemplate(event);
+  client.connect(function () {
+    client.join("##kdaigle-test", function() {
+      client.say("##kdaigle-test", compiledMessage);
+      client.part("##kdaigle-test");
+    });
+  });
+
+  client.addListener('error', function(message) {
+      console.log('error: ', message);
+  });
+}
---
+++ templates/issue.js
@@ -0,0 +1,3 @@
+module.exports = {
+  rawIssueTemplate: "[{{repository.name}}] {{sender.login}} {{action}} issue #{{issue.number}}: {{issue.title}}"
+}
